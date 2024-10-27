0787ec19f810240e31cc09dc6c772d701157c1ca
Dana-Sarkawt <dana.code01@gmail.com>
2024-10-27 14:52:52 +0300

    refactor: Update language settings for app.html and hooks.server.ts

--- src/routes/footer/+page.svelte
+++ src/routes/footer/+page.svelte
@@ -145,7 +145,7 @@
 </script>
 
 <div class="container mx-auto px-4 py-8">
-	<Card class="max-w-4xl mx-auto p-4 sm:p-6 bg-white shadow-lg rounded-lg">
+	<Card class="max-w-4xl mx-auto p-4 sm:p-6 bg-white dark:bg-main-dark shadow-lg rounded-lg">
 		<div class="flex mb-4 justify-between items-center">
 			<h1 class="text-2xl font-bold text-gray-800">{$_('footer-details')}</h1>
 			<div class="flex space-x-2">
@@ -174,7 +174,7 @@
 				<Label class="mb-2">{$_('phone-numbers')}</Label>
 				{#each footer.phones as phone, index}
 					<div class="flex items-center space-x-2 mb-2">
-						<Input type="tel" bind:value={footer.phones[index]} disabled={!isEditing} />
+						<Input type="tel" class="bg-input-light dark:bg-input-dark border-0" bind:value={footer.phones[index]} disabled={!isEditing} />
 						{#if isEditing}
 							<Button color="red" class="px-2 py-2" on:click={() => removePhone(index)}>
 								<Trash size="20" />
@@ -198,6 +198,7 @@
 							<Label class="ml-2 mb-1">{$_('name')}</Label>
 							<Input
 								type="text"
+								class="bg-input-light dark:bg-input-dark border-0"
 								bind:value={social.name}
 								placeholder={$_('social-name')}
 								disabled={!isEditing}
@@ -207,6 +208,7 @@
 							<Label class="ml-2 mb-1">{$_('link')}</Label>
 							<Input
 								type="url"
+								class="bg-input-light dark:bg-input-dark border-0"
 								bind:value={social.link}
 								placeholder={$_('social-link')}
 								disabled={!isEditing}
@@ -227,6 +229,7 @@
 								{#if isEditing}
 									<Input
 										type="file"
+										
 										accept="image/*"
 										class="absolute inset-0 opacity-0 cursor-pointer rounded-lg"
 										on:change={(e) => handleImageUpload(e, index)}
