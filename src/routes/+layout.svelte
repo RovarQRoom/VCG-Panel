2a2e0fe6cf6eca68b820cf64db3be2729dbcf18b
Dana-Sarkawt <dana.code01@gmail.com>
2024-10-27 09:43:31 +0300

    refactor: Update navigation menu with social page link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,15 +62,7 @@
 			class="bg-secondary-light dark:bg-secondary-dark text-primary-dark dark:text-primary-light p-2 rounded-full max-w-fit mx-auto mt-4 transition-all duration-300 ease-in-out"
 		>
 			<NavUl class="flex items-center space-x-4">
-				{#each [{ href: '/', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6', name: 'Home', field: '/' },
-				 { href: '/cards/1', icon: 'M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z', name: 'Cards', field: '/cards' },
-				  { href: '/carousels/1', icon: 'M4 6h16M4 10h16M4 14h16M4 18h16', name: 'Carousels', field: '/carousels' },
-				   { href: '/event', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', name: 'Event', field: '/event' },
-				    { href: '/representative', icon: 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z', name: 'Representative', field: '/representative' },
-					 { href: '/registrations/1', icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01', name: 'Registrations', field: '/registrations' },
-					 { href: '/footer', icon: 'M3 3h18v18H3V3zm4 2v14h2V5H7zm4 0v14h2V5h-2zm4 0v14h2V5h-2z', name: 'Footer', field: '/footer' },
-					 { href: '/social', icon: 'M12 2a10 10 0 100 20 10 10 0 000-20zm1 17.93A8.001 8.001 0 014 12h2a6 6 0 0011.93 1H13v2h4.93A8.001 8.001 0 0113 19.93zM12 4a8.001 8.001 0 017.93 7H13V9h4.93A8.001 8.001 0 0112 4z', name: 'Social', field: '/social' }
-					 ] as item}
+				{#each [{ href: '/', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6', name: 'Home', field: '/' }, { href: '/cards/1', icon: 'M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z', name: 'Cards', field: '/cards' }, { href: '/carousels/1', icon: 'M4 6h16M4 10h16M4 14h16M4 18h16', name: 'Carousels', field: '/carousels' }, { href: '/event', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', name: 'Event', field: '/event' }, { href: '/representative', icon: 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z', name: 'Representative', field: '/representative' }, { href: '/registrations/1', icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01', name: 'Registrations', field: '/registrations' }, { href: '/social', icon: 'M12 2a10 10 0 100 20 10 10 0 000-20zm1 17.93A8.001 8.001 0 014 12h2a6 6 0 0011.93 1H13v2h4.93A8.001 8.001 0 0113 19.93zM12 4a8.001 8.001 0 017.93 7H13V9h4.93A8.001 8.001 0 0112 4z', name: 'Social', field: '/social' }] as item}
 					<NavLi
 						href={item.href}
 						class="relative flex items-center transition-all duration-300 ease-in-out {isActive(
