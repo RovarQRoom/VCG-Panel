0cba7321252a411794de9f2f29389ea64fa47d11
RovarQRoom <rovar.07000120@outlook.com>
2024-11-03 10:00:46 +0300

    refactor: Update hover color in card component

--- src/routes/cards/[page]/+page.svelte
+++ src/routes/cards/[page]/+page.svelte
@@ -254,7 +254,7 @@
 				class="absolute top-2 right-10 bg-transparent"
 			>
 				<svg
-					class="w-5 h-5 text-gray-500 hover:text-blue-500"
+					class="w-5 h-5 hover:text-blue"
 					fill="none"
 					stroke="currentColor"
 					viewBox="0 0 24 24"
