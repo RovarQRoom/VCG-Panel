dc56a98c7c8f2e4a2bf6891711ea925e255499af
Dana-Sarkawt <dana.code01@gmail.com>
2024-11-07 10:03:59 +0300

    refactor: Update layout and styles in +page.svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,57 +17,58 @@
 	});
 </script>
 
-<div class="p-4 mt-24">
-	<div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+<div class="p-4 mt-24 h-auto">
+	<div class="p-4  rounded-lg mt-14">
+		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
 			<div
-				class="flex items-center justify-center h-24 rounded bg-blue text-white dark:bg-darkBlue"
+				class="flex items-center justify-center h-52 rounded bg-blue text-white dark:bg-darkBlue"
 			>
 				<p class="text-2xl text-gray-400 dark:text-gray-500">
 					<span class="font-semibold text-gray-900 dark:text-white">{totalCards}</span> Cards
 				</p>
 			</div>
 			<div
-				class="flex items-center justify-center h-24 rounded bg-blue text-white dark:bg-darkBlue"
+				class="flex items-center justify-center h-52 rounded bg-blue text-white dark:bg-darkBlue"
 			>
 				<p class="text-2xl text-gray-400 dark:text-gray-500">
 					<span class="font-semibold text-gray-900 dark:text-white">{totalEvents}</span> Events
 				</p>
 			</div>
-			<div
-				class="flex items-center justify-center h-24 rounded bg-blue text-white dark:bg-darkBlue"
-			>
-				<p class="text-2xl text-gray-400 dark:text-gray-500">
-					Welcome, <span class="font-semibold text-gray-900 dark:text-white">{userName}</span>
-				</p>
-			</div>
 		</div>
+
 		<div
+			class="flex items-center justify-center h-52 rounded bg-blue text-white dark:bg-darkBlue mb-4"
+		>
+			<p class="text-2xl text-gray-400 dark:text-gray-500">
+				Welcome, <span class="font-semibold text-gray-900 dark:text-white">{userName}</span>
+			</p>
+		</div>
+		<!-- <div
 			class="flex items-center justify-center h-48 mb-4 rounded bg-blue text-white dark:bg-darkBlue"
 		>
 			<p class="text-2xl text-gray-400 dark:text-gray-500">Quick Actions</p>
-		</div>
+		</div> -->
 		<div class="grid grid-cols-2 gap-4 mb-4">
-			<a href="/cards/1" class="text-xl text-blue-600 dark:text-blue-500">
+			<a href="/cards/1" class="h-52 text-xl text-blue-600 dark:text-blue-500">
 				<div
-					class="flex items-center justify-center rounded bg-blue text-white h-28 dark:bg-darkBlue hover:bg-blue/80 dark:hover:bg-darkBlue/80"
+					class="h-52 flex items-center justify-center rounded bg-blue text-white h-28 dark:bg-darkBlue hover:bg-blue/80 dark:hover:bg-darkBlue/80"
 				>
 					Manage Cards
 				</div>
 			</a>
 
-			<a href="/event" class="text-xl text-blue-600 dark:text-blue-500">
+			<a href="/event" class="h-52 text-xl text-blue-600 dark:text-blue-500">
 				<div
-					class="flex items-center justify-center rounded bg-blue text-white h-28 dark:bg-darkBlue hover:bg-blue/80 dark:hover:bg-darkBlue/80"
+					class="h-52 flex items-center justify-center rounded bg-blue text-white h-28 dark:bg-darkBlue hover:bg-blue/80 dark:hover:bg-darkBlue/80"
 				>
 					Manage Events
 				</div>
 			</a>
 		</div>
-		<div
+		<!-- <div
 			class="flex items-center justify-center h-48 mb-4 rounded bg-blue text-white dark:bg-darkBlue"
 		>
 			<p class="text-2xl text-gray-400 dark:text-gray-500">Recent Activity</p>
-		</div>
+		</div> -->
 	</div>
 </div>
